13c7d4d253935f7410b7f0a96f80316e0b8dd9bd
Park June Chul <pjc0247@naver.com>
2021-05-08 18:24:26 +0900

    aa

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
+import { Route, HashRouter as Router, Switch } from 'react-router-dom';
 
 import { Page1 } from './page/Page1';
 
